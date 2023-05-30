7370e7875e9ebe1d4fc453fcae744bddbb74e08e
SvyatLinux <swcnova@gmail.com>
2023-05-30 18:50:51 +0700

    fixed button value in popup

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -9,7 +9,7 @@ function PopupWithForm (props) {
               <button className="popup__close" type="button" aria-label="Закрыть картинку" onClick={props.onClose}/>
               <h2 className="popup-form__heading">{props.title}</h2>
               {props.children}
-              <input className="popup-form__btn" type="submit" defaultValue={props.buttonValue} />
+              <input className="popup-form__btn" type="submit" value={props.buttonValue} />
             </form>
         </div>
     </div>
